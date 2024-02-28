aed798667198b93a94476258af8e603cdac685f1
chrross <chrross@microsoft.com>
2024-02-27 16:34:07 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,44 @@
+using Microsoft.AspNetCore.DataProtection;
+using Microsoft.AspNetCore.Mvc;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDataProtection();
+var app = builder.Build();
+
+var instanceId = Guid.NewGuid();
+
+app.MapGet("/", () => "See /encrypt?input=Hello and /decrypt?input=[encrypted output]");
+
+app.MapGet("/encrypt", ([FromServices] IDataProtectionProvider provider, [FromQuery] string input) =>
+{
+    var protector = provider.CreateProtector("Test");
+    var output = "";
+    try
+    {
+        output = protector.Protect(input);
+    }
+    catch (Exception ex)
+    {
+        output = ex.ToString();
+    }
+
+    return new { input, output, instanceId };
+});
+
+app.MapGet("/decrypt", ([FromServices] IDataProtectionProvider provider, [FromQuery] string input) =>
+{
+    var protector = provider.CreateProtector("Test");
+    var output = "";
+    try
+    {
+        output = protector.Unprotect(input);
+    }
+    catch (Exception ex)
+    {
+        output = ex.ToString();
+    }
+
+    return new { input, output, instanceId };
+});
+
+app.Run();
